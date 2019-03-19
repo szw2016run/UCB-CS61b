41e7bf4cc4bd4e65952d83f487bf226cb888db2a
szw <41726196@xs.ustb.edu.cn>
2019-03-19 21:19:29 +0800

    completed first part of lab1

---
+++ lab1/HelloWorld.java
@@ -0,0 +1,17 @@
+public class HelloWorld{
+    public static void  main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("尚文静我爱你");
+        int i=1;
+        while(i<=5){
+            int j = 1;
+            while(j<=i){
+            System.out.print("*");
+                    j += 1;
+        }
+            i +=1;
+            System.out.println();
+
+    }
+}
+}
