ab84b55c6923a3eb3299b229379aefd70e6444bc
szw <41726196@xs.ustb.edu.cn>
2019-04-07 00:32:43 +0800

    Finished hw3

--- hw3/hw3/hash/OomageTestUtility.java
+++ hw3/hw3/hash/OomageTestUtility.java
@@ -1,5 +1,7 @@
 package hw3.hash;
 
+import javax.management.openmbean.OpenMBeanAttributeInfo;
+import java.util.ArrayList;
 import java.util.List;
 
 public class OomageTestUtility {
@@ -12,6 +14,21 @@ public class OomageTestUtility {
          * and ensure that no bucket has fewer than N / 50
          * Oomages and no bucket has more than N / 2.5 Oomages.
          */
+        ArrayList<Oomage> [] buckets = new ArrayList[M];
+        for (int i = 0; i < M; i++) {
+            buckets[i] = new ArrayList<Oomage>();
+        }
+
+        for (Oomage s : oomages) {
+            int bucketNumber = (s.hashCode() & 0x7FFFFFF) % M;
+            buckets[bucketNumber].add(s);
+        }
+
+        for(int i = 0; i < M; i++) {
+            if (buckets[i].size() >= oomages.size() / 50 && buckets[i].size() <= oomages.size() / 2.5){
+                return true;
+            }
+        }
         return false;
     }
 }
--- hw3/hw3/hash/TestComplexOomage.java
+++ hw3/hw3/hash/TestComplexOomage.java
@@ -1,5 +1,6 @@
 package hw3.hash;
 
+import edu.princeton.cs.algs4.StdRandom;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -20,7 +21,7 @@ public class TestComplexOomage {
     /* This should pass if your OomageTestUtility.haveNiceHashCodeSpread
        is correct. This is true even though our given ComplexOomage class
        has a flawed hashCode. */
-    /*@Test
+    @Test
     public void testRandomOomagesHashCodeSpread() {
         List<Oomage> oomages = new ArrayList<>();
         int N = 10000;
@@ -30,20 +31,27 @@ public class TestComplexOomage {
         }
 
         assertTrue(OomageTestUtility.haveNiceHashCodeSpread(oomages, 10));
-    }*/
+    }
 
     /* TODO: Create a list of Complex Oomages called deadlyList
      * that shows the flaw in the hashCode function.
      */
-    /*
     @Test
     public void testWithDeadlyParams() {
         List<Oomage> deadlyList = new ArrayList<>();
 
         // Your code here.
+        for (int i = 0; i < 20; i++) {
+            List<Integer> params = new ArrayList<>();
+            for (int k = 0; k < 8; k++) {
+                int stuff = StdRandom.uniform(200, 255);
+                params.add(stuff);
+            }
+            deadlyList.add(new ComplexOomage(params));
+        }
 
         assertTrue(OomageTestUtility.haveNiceHashCodeSpread(deadlyList, 10));
-    } */
+    }
 
     /** Calls tests for SimpleOomage. */
     public static void main(String[] args) {
